bd9fef59fb800548e78a334ca6d97283d8fd5896
MOHAN MUKUND SAI SUNKARA <116540844+SMMS28@users.noreply.github.com>
2023-12-24 21:57:47 +0530

    Update Hcode.c

--- CN-Lab-main/Hcode.c
+++ CN-Lab-main/Hcode.c
@@ -16,7 +16,6 @@ int main() {
     data[9]=data[0]^data[1]^data[4]^data[5]^data[8];
 	data[7]=data[4]^data[5]^data[6];
 	data[3]=data[0]^data[1]^data[2];
- 
 	printf("[Encoded Data] : ");
 	for(i=0;i<11;i++)
 	        printf("%d",data[i]);
